1c29445ba6164406a47cabb60b39fa18b4c6560e
Nishan Ahmed <nishanahmad@gmail.com>
2018-08-28 11:46:30 +0530

    Incoming sheet request V1

---
+++ Sheet/inbound.php
@@ -0,0 +1,15 @@
+<?php
+
+$area=$_GET['area'];
+$landmark=$_GET['landmark'];
+$location=$_GET['location'];
+$qty=$_GET['qty'];
+$contactName=$_GET['contactName'];
+$contactPhone=$_GET['contactPhone'];
+$customerName=$_GET['customerName'];
+$customerPhone=$_GET['customerPhone'];
+$date=$_GET['date'];
+$fe=$_GET['fe'];
+
+header("Location:http://nas.force.com/NavigateSite");						
+?>
