69c8186701e7b5919b049ea83fd695bcafbc8ae4
Nishan Ahmed <nishanahmad@gmail.com>
2018-09-06 10:34:20 +0530

    Cancel sheet request feature

--- Sheet/index.php
+++ Sheet/index.php
@@ -16,6 +16,7 @@ a{
 }
 </style>
 <title>SHEETS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" type="text/css" href="../css/index.css" />
 </head>
 <body>
