98e57b29ad753ca6abcb6aa45d84a52a4a6c15c7
Nishan Ahmed <nishanahmad@gmail.com>
2019-01-01 12:46:35 +0530

    CRON

--- cron/test.php
+++ cron/test.php
@@ -1,28 +1,32 @@
 <?php
 
-	$message = "Hello";
-	$phone = "9846086862";
-	
-	$curl = curl_init();
-	
-	curl_setopt_array($curl, array(
-			CURLOPT_URL => "https://control.msg91.com/api/postsms.php",
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_ENCODING => "",
-			CURLOPT_MAXREDIRS => 10,
-			CURLOPT_TIMEOUT => 30,
-			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			CURLOPT_CUSTOMREQUEST => "POST",
-			CURLOPT_POSTFIELDS => "<MESSAGE><AUTHKEY>212006AOQzrpS4jW5adee1be</AUTHKEY><ROUTE>AUTO</ROUTE><CAMPAIGN>ENGINEERS</CAMPAIGN><COUNTRY>91</COUNTRY><SENDER>ACCHLP</SENDER><SMS TEXT=\"".$message."\"><ADDRESS TO=\"".$phone."\"></ADDRESS></SMS></MESSAGE>",
-			CURLOPT_SSL_VERIFYHOST => 0,
-			CURLOPT_SSL_VERIFYPEER => 0,
-			CURLOPT_HTTPHEADER => array(
-			"content-type: application/xml"
-			),
-		)
-	);
+$message = "Hello";
 
-	$response = curl_exec($curl);
-	$err = curl_error($curl);
+$curl = curl_init();
 
-	curl_close($curl);
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://control.msg91.com/api/postsms.php",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "<MESSAGE> <AUTHKEY>212006AOQzrpS4jW5adee1be</AUTHKEY> <SENDER>SenderID</SENDER> <ROUTE>Template</ROUTE> <CAMPAIGN>SHEET</CAMPAIGN> <COUNTRY>91</COUNTRY> <SMS TEXT=\"".$message."\"> <ADDRESS TO=\"9846086862\"></ADDRESS> <ADDRESS TO=\"9567353448\"></ADDRESS> </SMS></MESSAGE>",
+  CURLOPT_SSL_VERIFYHOST => 0,
+  CURLOPT_SSL_VERIFYPEER => 0,
+  CURLOPT_HTTPHEADER => array(
+    "content-type: application/xml"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}	
