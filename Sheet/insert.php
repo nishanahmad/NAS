e171f3b3d70250ab5a3718abdf9e5cc98962886d
Nishan Ahmed <nishanahmad@gmail.com>
2019-01-02 16:51:27 +0530

    Sheet SMS

--- Sheet/insert.php
+++ Sheet/insert.php
@@ -1,6 +1,7 @@
 <?php
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+date_default_timezone_set("Asia/Kolkata");		
 
 require '../connect.php';
 session_start();
@@ -19,6 +20,42 @@ if(isset($_SESSION["user_name"]))
 
 	$result = mysqli_query($con, $sql) or die(mysqli_error($con));				 
 
+	$tomorrow = new DateTime('tomorrow');
+	$tomorrow = $tomorrow ->format('Y-m-d');
+
+	if($sqlDate == $tomorrow && time() > strtotime("12:00 PM"))
+	{
+		$message = $name." : ".$phone."&#xA;".$area.", ".$bags." bags&#xA;&#xA;";
+		
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => "https://control.msg91.com/api/postsms.php",
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => "",
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 30,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => "POST",
+		  CURLOPT_POSTFIELDS => "<MESSAGE> <AUTHKEY>212006AOQzrpS4jW5adee1be</AUTHKEY> <SENDER>ACCHLP</SENDER> <ROUTE>Template</ROUTE> <CAMPAIGN>SHEET</CAMPAIGN> <COUNTRY>91</COUNTRY> <SMS TEXT=\"".$message."\"> <ADDRESS TO=\"9744771145\"></ADDRESS> <ADDRESS TO=\"9567353448\"></ADDRESS> </SMS></MESSAGE>",
+		  CURLOPT_SSL_VERIFYHOST => 0,
+		  CURLOPT_SSL_VERIFYPEER => 0,
+		  CURLOPT_HTTPHEADER => array(
+			"content-type: application/xml"
+		  ),
+		));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) 
+			echo "cURL Error #:" . $err;
+		else
+			echo $response;		
+	}
+	
 	header( "Location: new.php" );
 
 }
--- test/main.php
+++ test/main.php
@@ -1,35 +1,15 @@
 <?php
-require '../connect.php';
-$prevTotal = -300;
-$newTotal =  500;
 
-$acheivedGold = 0;
-$totalGold = 0;
+date_default_timezone_set("Asia/Kolkata");		
 
+$sqlDate = date("Y-m-d",strtotime('2019-01-03'));
 
-if($prevTotal > 0 && $newTotal > 0)
+$tomorrow = new DateTime('tomorrow');
+$tomorrow = $tomorrow ->format('Y-m-d');
+
+if (time() > strtotime("4:48 PM")) 
 {
-	if(floor($prevTotal/600) < floor($newTotal/600))
-	{
-		$acheivedGold = floor($newTotal/600) - floor($prevTotal/600);
-		$totalGold = floor($newTotal/600);
-	}		
+	echo 'Hell Yeah';
 }
-else if($prevTotal < 0 && $newTotal > 0)
-{
-	if(floor($newTotal/600) > 0)
-	{
-		$acheivedGold = floor($newTotal/600);
-		$totalGold = floor($newTotal/600);
-	}				
-}
-
-//echo $acheivedGold;
-//echo '<br/>';
-//echo $totalGold;
-
-$sales = mysqli_query($con,"SELECT bill_no FROM nas_sale WHERE bill_no NOT LIKE 'a%' AND bill_no NOT LIKE 'A%' AND bill_no <> '' ORDER BY bill_no LIMIT 10") or die(mysqli_error($con));		 
-foreach($sales as $sale)
-{
-	var_dump($sale);
-}
+//if($sqlDate == $tomorrow)
+	//echo 'Hell Yeah';
