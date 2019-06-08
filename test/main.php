3ba681f811d1b86a4719b274266dbdabdeae6048
Nishan Ahmed <nishanahmad@gmail.com>
2019-06-08 15:52:14 +0530

    test

--- test/main.php
+++ test/main.php
@@ -1,7 +1,7 @@
 <?php
 require '../connect.php';
 
-$query ="select * from nas_sale where srp > 0 AND srh > 0";
+$query ="select * from nas_sale where srp > 0 AND f2r > 0";
 //$query ="select * from nas_sale where sales_id = 5835";
 $result = mysqli_query($con, $query) or die(mysqli_error($con));				 
 
@@ -12,7 +12,7 @@ foreach($result as $row)
   $ar = $row['ar_id'];
   $eng = $row['eng_id'];
   $truck = $row['truck_no'];
-  $srh = $row['srh'];
+  $f2r = $row['f2r'];
   $ret = $row['return_bag'];
   $bill = $row['bill_no'].'(1)';
   $cust = $row['customer_name'];
@@ -20,11 +20,11 @@ foreach($result as $row)
   $add1 = $row['address1'];
   $add2 = $row['address2'];
   
-	$sql="INSERT INTO nas_sale (entry_date, ar_id, eng_id, truck_no, srh, return_bag, bill_no, customer_name, customer_phone, address1, address2)
+	$sql="INSERT INTO nas_sale (entry_date, ar_id, eng_id, truck_no, f2r, return_bag, bill_no, customer_name, customer_phone, address1, address2)
 	VALUES
-	('$date', '$ar', ".var_export($eng, true).", '$truck', ".var_export($srh, true).", ".var_export($ret, true).", '$bill', '$cust', '$phone', '$add1', '$add2')";	  
+	('$date', '$ar', ".var_export($eng, true).", '$truck', ".var_export($f2r, true).", ".var_export($ret, true).", '$bill', '$cust', '$phone', '$add1', '$add2')";	  
 	
 	$insert = mysqli_query($con, $sql) or die(mysqli_error($con));	
 
-	$update = mysqli_query($con,"UPDATE nas_sale SET srh=null WHERE sales_id='$id'") or die(mysqli_error($con));	
+	$update = mysqli_query($con,"UPDATE nas_sale SET f2r=null WHERE sales_id='$id'") or die(mysqli_error($con));	
 }
