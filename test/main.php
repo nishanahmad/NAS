9da555dd62e7352655c5aa314af93f97a0775124
Nishan Ahmed <nishanahmad@gmail.com>
2019-06-10 09:47:16 +0530

    test

--- test/main.php
+++ test/main.php
@@ -1,13 +1,13 @@
 <?php
 require '../connect.php';
 
-$query ="select * from nas_sale where srp > 0 AND qty = 0 LIMIT 10000";
+$query ="select * from nas_sale where srh > 0 AND qty = 0 LIMIT 10000";
 
 $result = mysqli_query($con, $query) or die(mysqli_error($con));				 
 
 foreach($result as $row)
 {
 	$id = $row['sales_id'];
-	$qty = $row['srp'];
-	$update = mysqli_query($con,"UPDATE nas_sale SET brand=1,qty=$qty WHERE sales_id='$id'") or die(mysqli_error($con));	
+	$qty = $row['srh'];
+	$update = mysqli_query($con,"UPDATE nas_sale SET brand=2,qty=$qty WHERE sales_id='$id'") or die(mysqli_error($con));	
 }
