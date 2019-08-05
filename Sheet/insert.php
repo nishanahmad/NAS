c8932affa2f041c5cfd36f32eb82f792190fa231
Nishan Ahmed <nishanahmad@gmail.com>
2019-08-05 14:32:26 +0530

    Mnor

--- Sheet/insert.php
+++ Sheet/insert.php
@@ -15,48 +15,14 @@ if(isset($_SESSION["user_name"]))
 	$remarks = $_POST['remarks'];
 	$bags = (int)$_POST['bags'];
 	$requested_by = $_SESSION['user_name'];
+	$created_on = date('Y-m-d H:i:s');
 	
-	$sql="INSERT INTO sheets (date, name, phone, bags, area, shop, remarks, requested_by, status)
+	$sql="INSERT INTO sheets (date, name, phone, bags, area, shop, remarks, requested_by, status, created_on)
 		 VALUES
-		 ('$sqlDate', '$name', '$phone', $bags, '$area', '$shop', '$remarks', '$requested_by', 'requested')";
+		 ('$sqlDate', '$name', '$phone', $bags, '$area', '$shop', '$remarks', '$requested_by', 'requested', '$created_on')";
 
 	$result = mysqli_query($con, $sql) or die(mysqli_error($con));				 
 
-	$tomorrow = new DateTime('tomorrow');
-	$tomorrow = $tomorrow ->format('Y-m-d');
-
-	if($sqlDate == $tomorrow && time() > strtotime("12:00 PM"))
-	{
-		$message = $name." : ".$phone."&#xA;".$area.", ".$bags." bags&#xA;&#xA;";
-		
-		$curl = curl_init();
-
-		curl_setopt_array($curl, array(
-		  CURLOPT_URL => "https://control.msg91.com/api/postsms.php",
-		  CURLOPT_RETURNTRANSFER => true,
-		  CURLOPT_ENCODING => "",
-		  CURLOPT_MAXREDIRS => 10,
-		  CURLOPT_TIMEOUT => 30,
-		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		  CURLOPT_CUSTOMREQUEST => "POST",
-		  CURLOPT_POSTFIELDS => "<MESSAGE> <AUTHKEY>212006AOQzrpS4jW5adee1be</AUTHKEY> <SENDER>ACCHLP</SENDER> <ROUTE>Template</ROUTE> <CAMPAIGN>SHEET</CAMPAIGN> <COUNTRY>91</COUNTRY> <SMS TEXT=\"".$message."\"> <ADDRESS TO=\"9744771145\"></ADDRESS> <ADDRESS TO=\"9567353448\"></ADDRESS> </SMS></MESSAGE>",
-		  CURLOPT_SSL_VERIFYHOST => 0,
-		  CURLOPT_SSL_VERIFYPEER => 0,
-		  CURLOPT_HTTPHEADER => array(
-			"content-type: application/xml"
-		  ),
-		));
-
-		$response = curl_exec($curl);
-		$err = curl_error($curl);
-
-		curl_close($curl);
-
-		if ($err) 
-			echo "cURL Error #:" . $err;
-		else
-			echo $response;		
-	}
 	
 	header( "Location: requests.php" );
 
