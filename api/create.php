c9d6444b4928d5a81a6c2c1be79d9f487c2b6920
Nishan Ahmed <nishanahmad@gmail.com>
2019-09-05 09:13:04 +0530

    sheet

---
+++ api/create.php
@@ -0,0 +1,57 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ 
+include_once 'database.php';
+include_once 'sheet.php';
+ 
+$database = new Database();
+$db = $database->getConnection();
+ 
+$sheet = new Sheet($db);
+ 
+$data = json_decode(file_get_contents("php://input"));
+ 
+if(
+	!empty($data->date)&&
+    !empty($data->name) &&
+    !empty($data->phone) &&
+    !empty($data->bags) &&
+    !empty($data->area) &&
+	!empty($data->shop) &&
+	!empty($data->remarks) &&
+	!empty($data->requested_by)
+)
+{
+    $sheet->date = date('Y-m-d',strtotime($data->date));
+	$sheet->name = $data->name;
+    $sheet->phone = $data->phone;
+    $sheet->bags = $data->bags;
+    $sheet->area = $data->area;
+    $sheet->shop = $data->shop;
+	$sheet->remarks = $data->remarks;
+	$sheet->requested_by = $data->requested_by;
+	$sheet->created_on = date('Y-m-d');
+ 
+
+    if($sheet->create())
+	{
+        http_response_code(201);
+        echo json_encode(array("message" => "Sheet was created."));
+    }
+    else
+	{
+        http_response_code(503);
+        echo json_encode(array("message" => mysqli_error($database->conn->error)));
+    }
+}
+else
+{ 
+    http_response_code(400);
+    echo json_encode(array("message" => "Unable to create sheet. Data is incomplete."));
+}
+?>
---
+++ api/sheet.php
@@ -0,0 +1,65 @@
+<?php
+class Sheet
+{ 
+	private $conn;
+	private $table_name = "sheets";
+
+	public $date;
+	public $name;
+	public $phone;
+	public $bags;
+	public $area;
+	public $shop;
+	public $remarks;
+	public $requested_by;
+	public $created_on;
+
+	// constructor with $db as database connection
+	public function __construct($db)
+	{
+		$this->conn = $db;
+	}
+	
+	// create product
+	function create()
+	{ 
+		// query to insert record
+		$query = "INSERT INTO
+					" . $this->table_name . "
+				SET
+					date=:date, name=:name, phone=:phone, bags=:bags, area=:area, shop=:shop, remarks=:remarks, requested_by=:requested_by, status='pending', created_on=:created_on";
+	 
+		// prepare query
+		$stmt = $this->conn->prepare($query);
+	 
+		// sanitize
+		$this->date=htmlspecialchars(strip_tags($this->date));
+		$this->name=htmlspecialchars(strip_tags($this->name));
+		$this->phone=htmlspecialchars(strip_tags($this->phone));
+		$this->bags=htmlspecialchars(strip_tags($this->bags));
+		$this->area=htmlspecialchars(strip_tags($this->area));
+		$this->shop=htmlspecialchars(strip_tags($this->shop));
+		$this->remarks=htmlspecialchars(strip_tags($this->remarks));
+		$this->requested_by=htmlspecialchars(strip_tags($this->requested_by));
+		$this->created_on=htmlspecialchars(strip_tags($this->created_on));
+	 
+		// bind values
+		$stmt->bindParam(":date", $this->date);
+		$stmt->bindParam(":name", $this->name);
+		$stmt->bindParam(":phone", $this->phone);
+		$stmt->bindParam(":bags", $this->bags);
+		$stmt->bindParam(":area", $this->area);
+		$stmt->bindParam(":shop", $this->shop);
+		$stmt->bindParam(":remarks", $this->remarks);
+		$stmt->bindParam(":requested_by", $this->requested_by);
+		$stmt->bindParam(":created_on", $this->created_on);
+	 
+		// execute query
+		if($stmt->execute())
+		{
+			return true;
+		}
+	 
+		return false;
+	}	
+}
