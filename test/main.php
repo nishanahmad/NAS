b2e15ba0b0a08c63b832040487f434ab2f656cc8
Nishan Ahmed <nishanahmad@gmail.com>
2019-09-12 16:45:13 +0530

    Sheet

--- test/main.php
+++ test/main.php
@@ -1,10 +1,6 @@
 <?php
 require '../connect.php';
 
-$query ="select * from nas_sale where f2r > 0 AND qty = 0 LIMIT 10000";
+echo date('h:i');
 
-$result = mysqli_query($con, $query) or die(mysqli_error($con));				 
-
-$map = array();
-
-$map['2019-06-01']['hi'] = 0;
+echo date('h:i A');
