8cd07a1184fb44c33b20eac2eb0f9cf06dc89fd8
Nishan Ahmed <nishanahmad@gmail.com>
2020-02-04 15:45:33 +0530

    Rework on all sales

--- sales/list.php
+++ sales/list.php
@@ -1,121 +1,122 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 session_start();
 if(isset($_SESSION["user_name"]))
 {
-?>	
+	require '../connect.php';
+	
+	if(isset($_POST['fromDate']))
+		$fromDate = date('Y-m-d',strtotime($_POST['fromDate']));
+	else
+	{
+		$date = new DateTime('FIRST DAY OF PREVIOUS MONTH');
+		$fromDate = $date->format('Y-m-d');
+	}
+	if(isset($_POST['toDate']))
+		$toDate = date('Y-m-d',strtotime($_POST['toDate']));
+	else
+		$toDate = date('Y-m-d');
+		
+	$sales = mysqli_query($con,"SELECT * FROM nas_sale WHERE entry_date >= '$fromDate' AND entry_date <= '$toDate' ORDER BY sales_id DESC") or die(mysqli_error($con));
+	
+
+	$arObjects = mysqli_query($con,"SELECT * FROM ar_details") or die(mysqli_error($con));
+	foreach($arObjects as $ar)
+		$arMap[$ar['id']] = $ar['name'];
+
+	$productObjects = mysqli_query($con,"SELECT * FROM products") or die(mysqli_error($con));
+	foreach($productObjects as $product)
+		$productMap[$product['id']] = $product['name'];																																			?>
+		
 <!DOCTYPE html>
 <html>
+<head>
 	<title>Sales List</title>
-	<head>
-	<style>
-.dataTables_wrapper .dt-buttons {
-  float:none;  
-  text-align:center;
-}
-	</style>	
-		<link rel="stylesheet" type="text/css" href="../css/glow_box.css">
-		<link rel="stylesheet" type="text/css" href="../css/jquery.dataTables.css">
-		<link rel="stylesheet" type="text/css" href="../css/fixedHeader.css">
-		<link rel="stylesheet" type="text/css" href="../css/buttons.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">	
+	<link rel="stylesheet" type="text/css" href="../css/jquery-ui.css">
+	<script type="text/javascript" language="javascript" src="../js/jquery.js"></script>
+	<script type="text/javascript" language="javascript" src="../js/jquery-ui.min.js"></script>	
+	<script type="text/javascript" language="javascript" src="../js/TableSorter.js"></script>
+	<script type="text/javascript" language="javascript" src="../js/TablesorterWidgets.js"></script>
+	<link rel="stylesheet" type="text/css" href="../css/TableSorterBlueTheme.css">
+</head>
+<body>
+	<div align="center">
+		<a href="../index.php" class="link"><img alt='home' title='home' src='../images/home.png' width='60px' height='60px'/> </a> &nbsp;&nbsp;&nbsp;
+		<a href="new.php" class="link"><img alt='Add' title='Add New' src='../images/addnew.png' width='60px' height='60px'/></a>
+	</div>
+	<div align="center">
+		<br/><br/>
+		<form method="post" action="" autocomplete="off">
+			<input type="text" id="fromDate" class="textarea" name="fromDate" required value="<?php echo date('d-m-Y',strtotime($fromDate)); ?>" />
+			&nbsp;&nbsp;to&nbsp;&nbsp;
+			<input type="text" id="toDate" class="textarea" name="toDate" required value="<?php echo date('d-m-Y',strtotime($toDate)); ?>" />
+			<br/><br/>
+			<input type="submit" class="btn btn-primary" name="submit" value="Update">	
+			<br/><br/><br/>
+		</form>	
+		<font size="5"><b>TOTAL : <span class='total'></span></b></font>
+		<br/><br/>
+		<table id="sales-table" style="width:95%;">
+			<thead>
+				<tr>
+					<th style="width:100px;">Date</th>
+					<th>AR</th>
+					<!--th>Rate</th-->	
+					<th style="width:30px;">Product</th>
+					<th style="width:30px;">Qty</th>
+					<th>Bill</th>							
+					<th>Truck</th>
+					<th>Customer</th>							
+					<th>Engineer</th>							
+					<th>Remarks</th>							
+				</tr>
+			</thead>
+			<tbody><?php
+			foreach($sales as $sale)
+			{?>
+				<tr>
+					<td><a href="edit.php?sales_id=<?php echo $sale['sales_id'];?>"</a><?php echo date('d-m-Y',strtotime($sale['entry_date']));?></td>
+					<td><?php echo $arMap[$sale['ar_id']];?></td>
+					<td><?php echo $productMap[$sale['product']];?></td>
+					<td><?php echo $sale['qty'];?></td>
+					<td><?php echo $sale['bill_no'];?></td>
+					<td><?php echo $sale['truck_no'];?></td>
+					<td><?php echo $sale['customer_name'];?></td>
+					<td><?php if(!empty($sale['eng_id'])) echo $arMap[$sale['eng_id']];?></td>
+					<td><?php echo $sale['remarks'];?></td>
+				</tr><?php
+			}?>
+			</tbody>
+		</table>
+	</div>
+	<script>        
+	$(function(){
 
-		<script type="text/javascript" language="javascript" src="../js/jquery.js"></script>
-		<script type="text/javascript" language="javascript" src="../js/jquery.dataTables.js"></script>
-		<script type="text/javascript" language="javascript" src="../js/fixedHeader.js"></script>
-		<script type="text/javascript" language="javascript" src="../js/buttons.js"></script>
-		<script type="text/javascript" language="javascript" src="../js/html5ExportButton.js"></script>
-		<script type="text/javascript" language="javascript" src="../js/colVis.js"></script>	
-		<script type="text/javascript" language="javascript" src="../js/jsZip.js"></script>		
-		<script type="text/javascript" language="javascript">
-			$(document).ready(function() {
-				var dataTable = $('#sales-table').DataTable( {
-					dom: 'lfBrtip',
-					buttons: ['excelHtml5','colvis'],									
-					"processing": true,
-					"serverSide": true,
-					"responsive": true,
-					"bJQueryUI":true,
-					"iDisplayLength": 500,	
-					"aaSorting" : [[0, 'desc']],					
-					"ajax":{
-						url :"list_server.php", // json datasource
-						type: "post",  // method  , by default get
-						error: function(){  // error handling
-							$(".sales-table-error").html("");
-							$("#sales-table").append('<tbody class="sales-table-error"><tr><th colspan="3">No data found in the server</th></tr></tbody>');
-							$("#sales-table_processing").css("display","none");
-										}
-						   }
-				} );
-				
-   dataTable.on( 'xhr', function () {
-    var json = dataTable.ajax.json();
-	$('.total').html(json.total);
-	$('.sql').html(json.sql);
-} );				
-				
-				
-				$("#employee-grid_filter").css("display","none");  // hiding global search box
-				$('.search-input-text').on( 'keyup click', function () {   // for text boxes
-					var i =$(this).attr('data-column');  // getting column index
-					var v =$(this).val();  // getting search input value
-					dataTable.columns(i).search(v).draw();
-				} );
-				$('.search-input-select').on( 'change', function () {   // for select box
-					var i =$(this).attr('data-column');  
-					var v =$(this).val();  
-					dataTable.columns(i).search(v).draw();
-				} );
-				
-			} );
-		</script>
+		$('table').on('initialized filterEnd', function(){
+			var total = 0;
+			$(this).find('tbody tr:visible').each(function(){
+				total += parseFloat( $(this).find('td:eq(3)').text() );
+			});
+			$('.total').text(total);
+		})      
+	
+		$("table").tablesorter({
+			dateFormat : "ddmmyyyy",
+			theme : 'blue',
+			widgets: ['filter'],
+			filter_columnAnyMatch: true,
+		}); 
 
-	</head>
-	<body>
-		<div align="center">
-					<a href="../index.php" class="link"><img alt='home' title='home' src='../images/home.png' width='60px' height='60px'/> </a> &nbsp;&nbsp;&nbsp;
-					<a href="new.php" class="link"><img alt='Add' title='Add New' src='../images/addnew.png' width='60px' height='60px'/></a>
-		
-		</div>
-<div align="center" class="gradient">
-<font size="5">
-<br>
-<!--SQL:<span class='sql'></span><br><br-->
-</br>
-<b>TOTAL : <span class='total'></span>
-</b></font>
-		<br><br><br><br>
-			<input type="text" data-column="0" style="width:50px" class="search-input-text textarea" placeholder="Id">&nbsp&nbsp
-			<input type="text" data-column="1"  class="search-input-text textarea" placeholder="Date">&nbsp&nbsp
-			<input type="text" data-column="2"  class="search-input-text textarea" placeholder="AR">&nbsp&nbsp
-			<input type="text" data-column="3" style="width:50px" class="search-input-text textarea" placeholder="Product">&nbsp&nbsp
-			<input type="text" data-column="4" style="width:50px" class="search-input-text textarea" placeholder="Qty">&nbsp&nbsp
-			<input type="text" data-column="5"  class="search-input-text textarea" placeholder="Bill">&nbsp&nbsp
-			<input type="text" data-column="6"  class="search-input-text textarea" placeholder="Truck">&nbsp&nbsp
-			<input type="text" data-column="7"  class="search-input-text textarea" placeholder="Customer">&nbsp&nbsp
-			<input type="text" data-column="8"  class="search-input-text textarea" placeholder="Engineer">&nbsp&nbsp
-			<input type="text" data-column="9"  class="search-input-text textarea" placeholder="Remarks">
-
-		<br><br>
-			<table id="sales-table" class="display cell-border no-wrap" >
-					<thead>
-						<tr>
-							<th>Id</th>
-							<th style="min-width:90px !important">Date</th>
-							<th style="width:200px !important">AR</th>
-							<!--th>Rate</th-->	
-							<th>Product</th>
-							<th>Qty</th>
-							<th>Bill</th>							
-							<th>Truck</th>
-							<th>Customer</th>							
-							<th>Engineer</th>							
-							<th>Remarks</th>							
-						</tr>
-					</thead>
-			</table>
-		</div>
-	</body>
-</html>																				<?php
+		var pickerOpts = { dateFormat:"dd-mm-yy"}; 
+		$( "#fromDate" ).datepicker(pickerOpts);
+		$( "#toDate" ).datepicker(pickerOpts);				
+	});
+	</script>       	
+</body>
+</html>																				<?php																				
+	mysqli_close($con);
 }
 else
 	header("Location:../index.php");
