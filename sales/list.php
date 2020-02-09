ddc58e33a2749465fcf80905b18e92c44a6e50b7
Nishan Ahmed <nishanahmad@gmail.com>
2020-02-09 15:39:22 +0530

    Ar select List

--- sales/list.php
+++ sales/list.php
@@ -17,11 +17,19 @@ if(isset($_SESSION["user_name"]))
 		$toDate = date('Y-m-d',strtotime($_POST['toDate']));
 	else
 		$toDate = date('Y-m-d');
-		
-	$sales = mysqli_query($con,"SELECT * FROM nas_sale WHERE entry_date >= '$fromDate' AND entry_date <= '$toDate' ORDER BY sales_id DESC") or die(mysqli_error($con));
 	
+	if(isset($_POST['arId']))
+	{
+		$arId = $_POST['arId'];
+		$sales = mysqli_query($con,"SELECT * FROM nas_sale WHERE (ar_id = $arId OR eng_id = $arId) AND entry_date >= '$fromDate' AND entry_date <= '$toDate' ORDER BY sales_id DESC") or die(mysqli_error($con));		
+	}
+	else
+	{
+		$arId = 'All';
+		$sales = mysqli_query($con,"SELECT * FROM nas_sale WHERE entry_date >= '$fromDate' AND entry_date <= '$toDate' ORDER BY sales_id DESC") or die(mysqli_error($con));		
+	}	
 
-	$arObjects = mysqli_query($con,"SELECT * FROM ar_details") or die(mysqli_error($con));
+	$arObjects = mysqli_query($con,"SELECT * FROM ar_details ORDER BY name") or die(mysqli_error($con));
 	foreach($arObjects as $ar)
 		$arMap[$ar['id']] = $ar['name'];
 
@@ -36,10 +44,12 @@ if(isset($_SESSION["user_name"]))
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">	
 	<link rel="stylesheet" type="text/css" href="../css/jquery-ui.css">
 	<script type="text/javascript" language="javascript" src="../js/jquery.js"></script>
-	<script type="text/javascript" language="javascript" src="../js/jquery-ui.min.js"></script>	
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
 	<script type="text/javascript" language="javascript" src="../js/TableSorter.js"></script>
 	<script type="text/javascript" language="javascript" src="../js/TablesorterWidgets.js"></script>
+	<link href='../select2/dist/css/select2.min.css' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="../css/TableSorterBlueTheme.css">
+	<script src='../select2/dist/js/select2.min.js' type='text/javascript'></script>
 </head>
 <body>
 	<div align="center">
@@ -48,12 +58,23 @@ if(isset($_SESSION["user_name"]))
 	</div>
 	<div align="center">
 		<br/><br/>
-		<form method="post" action="" autocomplete="off">
-			<input type="text" id="fromDate" class="textarea" name="fromDate" required value="<?php echo date('d-m-Y',strtotime($fromDate)); ?>" />
-			&nbsp;&nbsp;to&nbsp;&nbsp;
-			<input type="text" id="toDate" class="textarea" name="toDate" required value="<?php echo date('d-m-Y',strtotime($toDate)); ?>" />
+		<form method="post" action="" autocomplete="off" style="width:30%;">
+			
+			<select name="arId" id="arId" class="form-control">
+				<option value = "" <?php if($arId == 'All') echo 'selected';?> >ALL</option>													    	<?php
+				foreach($arMap as $id => $name)
+				{																																			?>
+					<option value="<?php echo $id;?>" <?php if($arId == $id) echo 'selected';?>><?php echo $name;?></option> 						<?php
+				}																																			?>
+			</select>
 			<br/><br/>
-			<input type="submit" class="btn btn-primary" name="submit" value="Update">	
+			<div class="input-group">
+				<input type="text" id="fromDate" class="form-control" name="fromDate" required value="<?php echo date('d-m-Y',strtotime($fromDate)); ?>"/>
+				<span class="input-group-addon"> to </span>
+				<input type="text" id="toDate" class="form-control" name="toDate" required value="<?php echo date('d-m-Y',strtotime($toDate)); ?>"/>					
+			</div>
+			<br/><br/>
+			<input type="submit" class="btn btn-primary" name="submit" value="Update">
 			<br/><br/><br/>
 		</form>	
 		<font size="5"><b>TOTAL : <span class='total'></span></b></font>
@@ -93,7 +114,8 @@ if(isset($_SESSION["user_name"]))
 	</div>
 	<script>        
 	$(function(){
-
+		$("#arId").select2();
+		
 		$('table').on('initialized filterEnd', function(){
 			var total = 0;
 			$(this).find('tbody tr:visible').each(function(){
