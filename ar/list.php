17300a41292526355d494d9b284e8d70a3c4ffd2
Nishan Ahmed <nishanahmad@gmail.com>
2020-03-17 20:31:11 +0530

    TableSorter

--- ar/list.php
+++ ar/list.php
@@ -10,10 +10,14 @@ if(isset($_SESSION["user_name"]))
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" type="text/css" href="../css/loader.css">
-<link rel="stylesheet" type="text/css" href="../css/responstable.css">
-<link href="../css/font-awesome.min.css" rel="stylesheet">		
 <script type="text/javascript" language="javascript" src="../js/jquery.js"></script>
+<script type="text/javascript" language="javascript" src="../js/jquery-ui.min.js"></script>
+<link href="../css/bootstrap.min.css" rel="stylesheet">
+<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="../css/jquery-ui.css">
+<script src="../js/TableSorter.js"></script>
+<script src="../js/TablesorterWidgets.js"></script>	
+<link rel="stylesheet" href="../css/TableSorterBlueTheme.css">						
 <title>AR List</title>
 </head>
 <body>
@@ -22,12 +26,14 @@ if(isset($_SESSION["user_name"]))
 <a href="../index.php" class="link"><img alt='home' title='home' src='../images/home.png' width='60px' height='60px'/> </a>
 <br><br>
 </div>
-<table align="center" class="responstable" style="width:90%;">
+<div align="center">
+<table style="width:60%;">
 <?php
 	$sql = "SELECT * FROM ar_details WHERE type LIKE '%AR%' ORDER BY name ASC ";
 	$result = mysqli_query($con, $sql) or die(mysqli_error($con));?>
+	<thead>
 	<tr>
-		<th style="width:3%"></th>
+		<th style="width:3%">Id</th>
 		<th style="width:20%">Name</th>
 		<th style="width:20%">Shop</th>
 		<th style="text-align:center;width:8%">SAP</th>
@@ -35,6 +41,8 @@ if(isset($_SESSION["user_name"]))
 		<th>Area</th>
 		<th>Status</th>
 	</tr>
+	</thead>
+	<tbody>
 	<?php
 	while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) 
 	{
@@ -47,7 +55,7 @@ if(isset($_SESSION["user_name"]))
 		$status = $row['isActive'];
 	?>	
 	<tr>
-		<td><a style="color:grey;" href="view.php?id=<?php echo $row['id'];?>"><i class="fa fa-pencil"></a></td>	
+		<td><a style="color:grey;" href="view.php?id=<?php echo $row['id'];?>"><?php echo $row['id'];?></a></td>	
 		<td><?php echo $arname; ?></td>	
 		<td><?php echo $shopName; ?></td>	
 		<td style="text-align:center;width:8%"><label align="center"><?php echo $sapCode; ?></td>	
@@ -56,11 +64,24 @@ if(isset($_SESSION["user_name"]))
 		<td style="text-align:center;width:8%"><?php if($status == 1 ) echo 'Active'; else echo 'InActive';?></td>
 	</tr>																													<?php
 	}																																																										?>
+	</tbody>	
 </table>
-<br><br>
-<div align="center"><input type="submit" name="submit" value="Submit" onclick=" return showLoader()"></div>		
+</div>
+<br/><br/>
+<div align="center"><input type="submit" name="submit" value="Submit" onclick=" return showLoader()"></div>
+<br/><br/>
 </div>
 </body>
+<script>
+$(document).ready(function() {		
+	$("table").tablesorter({
+		dateFormat : "ddmmyyyy",
+		theme : 'blue',
+		widgets: ['filter'],
+		filter_columnAnyMatch: true,
+	}); 
+} );
+</script>
 </html>																														<?php
 
 }
