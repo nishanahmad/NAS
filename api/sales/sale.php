75a665e304024b71171853b58db550be6d1a9a14
Nishan Ahmed <nishanahmad@gmail.com>
2020-06-18 11:55:43 +0530

    Api changes

--- api/sales/sale.php
+++ api/sales/sale.php
@@ -4,33 +4,27 @@ class Sale
 	private $conn;
 	private $table_name = "nas_sale";
 
-	public $sales_id;
-	public $entry_date;
-	public $product;
-	public $qty;
-	public $ar;	
-	public $shop;	
-	public $eng_id;
-	public $return_bag;
-	public $discount;
-	public $remarks;
-	public $customer_name;
-	public $customer_phone;
-	public $address1;
-	public $address2;
-
 	// constructor with $db as database connection
 	public function __construct($db)
 	{
 		$this->conn = $db;
 	}
 	
-	function getDailySales($today)
+	function getDailySales($day)
 	{
-		$query = "SELECT * FROM ".$this->table_name." WHERE entry_date = '$today'";
+		$query = "SELECT * FROM ".$this->table_name." WHERE entry_date = '$day'";
 		$stmt = $this->conn->prepare($query);
 		$stmt->execute();
 	 
 		return $stmt;
 	}	
+	
+	function getDailySaleSum($day)
+	{
+		$query = "SELECT SUM(qty),product FROM ".$this->table_name." WHERE entry_date = '$day' GROUP BY product";
+		$stmt = $this->conn->prepare($query);
+		$stmt->execute();
+	 
+		return $stmt;
+	}		
 }
