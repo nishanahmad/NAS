7f696d57ca5432dcd6d4d3b6328ca4c4a7a19877
Nishan Ahmed <nishanahmad@gmail.com>
2020-08-07 16:10:44 +0530

    Auto populate truck details for Sale

--- sales/ajax/getDriverPhone.php
+++ sales/ajax/getDriverPhone.php
@@ -3,9 +3,10 @@
 	
 	if(!empty($_POST['truck']))
 	{
-		$truckNumber = $_POST['truck'];
+		preg_match_all('!\d+!', $_POST['truck'], $truckArray);
+		$truckNumber = $truckArray[0][0];
 		
-		$query = mysqli_query($con,"SELECT phone FROM truck_details WHERE number = '$truckNumber'") or die(mysqli_error($con));
+		$query = mysqli_query($con,"SELECT phone FROM truck_details WHERE number = '$truckNumber' ") or die(mysqli_error($con));
 		if(mysqli_num_rows($query)>0)
 		{
 			$truck = mysqli_fetch_array($query,MYSQLI_ASSOC) or die(mysqli_error($con));				 	 
--- test/sms.php
+++ test/sms.php
@@ -1,33 +1,5 @@
 <?php
 
-$curl = curl_init();
-
-$contacts = "9846086862,7902569643";
-
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.msg91.com/api/v2/sendsms?country=91",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "{ \"sender\": \"SOCKET\", \"route\": \"4\", \"country\": \"91\", \"sms\": [ { \"message\": \"Message1\", \"to\": [ ".$contacts." ] }] }",
-  CURLOPT_SSL_VERIFYHOST => 0,
-  CURLOPT_SSL_VERIFYPEER => 0,
-  CURLOPT_HTTPHEADER => array(
-    "authkey: 212006AOQzrpS4jW5adee1be",
-    "content-type: application/json"
-  ),
-));
-
-$response = curl_exec($curl);
-$err = curl_error($curl);
-
-curl_close($curl);
-
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  echo $response;
-}
+	preg_match_all('!\d+!', '1458/Vp', $truckArray);
+	$truckNumber = $truckArray[0][0];
+	var_dump($truckNumber);
