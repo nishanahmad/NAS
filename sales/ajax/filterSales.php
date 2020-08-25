fdc64d1eb59ab6212cce9274d3458dbd8cc0d82d
Nishan Ahmed <nishanahmad@gmail.com>
2020-08-25 12:44:50 +0530

    Bill no asc for today list

--- sales/ajax/filterSales.php
+++ sales/ajax/filterSales.php
@@ -32,7 +32,10 @@
 		$phone = $_POST['phone'];
 		$sql = $sql." AND customer_phone = '$phone'";
 	}		
-
+	
+	if($startDate == $endDate && $startDate == date('Y-m-d'))
+		$sql = $sql." ORDER BY bill_no ASC";
+	
 	$result=mysqli_query($con,$sql);
 	$rowcount=mysqli_num_rows($result);
 	
