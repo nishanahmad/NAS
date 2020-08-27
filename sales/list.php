0685325bf4a0f171ec02e15862e6d8e3836eb0e0
Nishan Ahmed <nishanahmad@gmail.com>
2020-08-28 00:42:58 +0530

    New Sale Form made responsive

--- sales/list.php
+++ sales/list.php
@@ -64,6 +64,7 @@ if(isset($_SESSION["user_name"]))
 
 <html>
 	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="../css/styles.css" rel="stylesheet" type="text/css">	
 		<link href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" rel="stylesheet" type="text/css">
 		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
