5d9d787f85c891fd932466662e6b34df3d8c8c53
Nishan Ahmed <nishanahmad@gmail.com>
2020-08-29 16:03:18 +0530

    Responsive sales list

--- sales/list.php
+++ sales/list.php
@@ -70,12 +70,16 @@ if(isset($_SESSION["user_name"]))
 		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.3/js/jquery.tablesorter.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.3/js/jquery.tablesorter.widgets.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.9.1/jquery.tablesorter.min.js" integrity="sha512-mWSVYmb/NacNAK7kGkdlVNE4OZbJsSUw8LiJSgGOxkb4chglRnVfqrukfVd9Q2EOWxFp4NfbqE3nDQMxszCCvw==" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.9.1/jquery.tablesorter.widgets.min.js" integrity="sha512-6I1SQyeeo+eLGJ9aSsU43lGT+w5HYY375ev/uIghqqVgmSPSDzl9cqiQC4HD6g8Ltqz/ms1kcf0takjBfOlnig==" crossorigin="anonymous"></script>
 		<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet"/>
 		<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
 		<title>Sales</title>
 		<style>
+			@media only screen and (max-width: 900px) {
+				.desktop-only{display: none;}	
+			}
+		
 			.select2-selection__rendered {
 				line-height: 33px !important;
 			}
@@ -85,12 +89,64 @@ if(isset($_SESSION["user_name"]))
 			.select2-selection__arrow {
 				height: 37px !important;
 			}
-			table.ui-table-reflow thead { display: none; }
+
+			/* REQUIRED CSS: change your reflow breakpoint here (35em below) */
+			@media ( max-width: 35em ) {
+
+			  table.ui-table-reflow thead { display: none; }
+
+			  /* css for reflow & reflow2 widgets */
+			  .ui-table-reflow td,
+			  .ui-table-reflow th {
+				-webkit-box-sizing: border-box;
+				-moz-box-sizing: border-box;
+				box-sizing: border-box;
+				float: right;
+				/* if not using the stickyHeaders widget (not the css3 version)
+				 * the "!important" flag, and "height: auto" can be removed */
+				width: 100% !important;
+				height: auto !important;
+			  }
+
+			  /* reflow widget only */
+			  .ui-table-reflow tbody td[data-title]:before {
+				color: #469;
+				font-size: .9em;
+				content: attr(data-title);
+				float: left;
+				width: 50%;
+				white-space: pre-wrap;
+				text-align: bottom;
+				display: inline-block;
+			  }
+
+			  /* reflow2 widget only */
+			  table.ui-table-reflow .ui-table-cell-label.ui-table-cell-label-top {
+				display: block;
+				padding: .4em 0;
+				margin: .4em 0;
+				text-transform: uppercase;
+				font-size: .9em;
+				font-weight: 400;
+			  }
+			  table.ui-table-reflow .ui-table-cell-label {
+				padding: .4em;
+				min-width: 30%;
+				display: inline-block;
+				margin: -.4em 1em -.4em -.4em;
+			  }
+
+			} /* end media query */
+
+			/* reflow2 widget */
+			.ui-table-reflow .ui-table-cell-label {
+			  display: none;
+			}			
 		</style>			
 	</head>
 	<body>
 		<nav class="navbar navbar-light bg-light sticky-top bottom-nav">
-			<div class="btn-group" role="group" aria-label="Button group with nested dropdown" style="float:left;margin-left:20px;">
+			<div class="btn-group" role="group" aria-label="Button group with nested dropdown" style="float:left;margin-left:2%;">
 				<div class="btn-group" role="group">
 					<button id="btnGroupDrop1" type="button" class="btn btn-outline-success dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
 						<i class="far fa-calendar-alt"></i> <?php echo $range;?>
@@ -103,7 +159,7 @@ if(isset($_SESSION["user_name"]))
 				</div>
 			</div>					
 			<span class="navbar-brand" style="font-size:25px;"><i class="fa fa-bolt"></i> Sales</span>
-			<a href="#" class="btn btn-sm" role="button" style="background-color:#54698D;color:white;float:right;margin-right:40px;" data-toggle="modal" data-target="#saleModal"><i class="fa fa-bolt"></i> New Sale</a>			
+			<a href="#" class="btn btn-sm" role="button" style="background-color:#54698D;color:white;float:right;margin-right:3%;" data-toggle="modal" data-target="#saleModal"><i class="fa fa-bolt"></i> New Sale</a>			
 		</nav>
 		<div style="width:100%;" class="mainbody">	
 			<div id="snackbar"><i class="fa fa-bolt"></i>&nbsp;&nbsp;Sale detail saved successfully !!!</div>
@@ -149,7 +205,7 @@ if(isset($_SESSION["user_name"]))
 				</table>
 			</div>
 			<br/><br/>
-			<table class="maintable table table-hover table-bordered" style="width:95%;margin-left:40px;">
+			<table class="maintable table table-hover table-bordered ui-table-reflow" style="width:95%;margin-left:2%;">
 				<thead>
 					<tr class="table-success">
 						<th style="width:110px;"><i class="far fa-calendar-alt"></i> Date</th>
@@ -157,10 +213,10 @@ if(isset($_SESSION["user_name"]))
 						<th style="width:80px;"><i class="fa fa-shield"></i> PROD</th>
 						<th style="width:80px;"><i class="fab fa-buffer"></i> QTY</th>
 						<th style="width:80px;"><i class="fa fa-rupee-sign"></i> RATE</th>
-						<th style="width:120px;"><i class="far fa-file-alt"></i> BILL NO</th>
-						<th style="width:120px;" class="desktop-only" ><i class="fas fa-truck-moving"></i> TRUCK</th>
+						<th style="width:120px;" class="desktop-only"><i class="far fa-file-alt"></i> BILL NO</th>
+						<th style="width:120px;" class="desktop-only"><i class="fas fa-truck-moving"></i> TRUCK</th>
 						<th style="width:120px;"><i class="far fa-user"></i> CUSTOMER</th>
-						<th><i class="far fa-comment-dots"></i> REMARKS</th>
+						<th class="desktop-only"><i class="far fa-comment-dots"></i> REMARKS</th>
 						<th class="desktop-only"><i class="fas fa-map-marker-alt"></i> ADDRESS</th>
 					</tr>	
 				</thead>
@@ -188,16 +244,16 @@ if(isset($_SESSION["user_name"]))
 						$finalRate = $rate - $cd - $wd - $sale['discount'];																					?>	
 						
 						<tr data-id="<?php echo $sale['id'];?>" data-params="<?php echo explode('?',$_SERVER['REQUEST_URI'])[1];?>" class="saleId" style="cursor:pointer;">
-							<td><?php echo date('d-m-Y',strtotime($sale['date'])); ?></td>
-							<td><?php echo $clientNamesMap[$sale['client']]; ?></td>
-							<td><?php echo $productNamesMap[$sale['product']];?></td>
-							<td><?php echo $sale['qty']; ?></td>
-							<td><?php echo $finalRate.'/-';?></td>							
-							<td><?php echo $sale['bill']; ?></td>
-							<td class="desktop-only"><?php echo $sale['truck']; ?></td>
-							<td><?php echo $sale['name'].'<br/>'.$sale['phone']; ?></td>
-							<td><?php echo $sale['remarks']; ?></td>
-							<td class="desktop-only"><?php echo $sale['address']; ?></td>
+							<td data-title="Date"><?php echo date('d-m-Y',strtotime($sale['date'])); ?></td>
+							<td data-title="AR"><?php echo $clientNamesMap[$sale['client']]; ?></td>
+							<td data-title="Product"><?php echo $productNamesMap[$sale['product']];?></td>
+							<td data-title="Qty"><?php echo $sale['qty']; ?></td>
+							<td data-title="Rate"><?php echo $finalRate.'/-';?></td>							
+							<td data-title="Bill" class="desktop-only"><?php echo $sale['bill']; ?></td>
+							<td data-title="Truck" class="desktop-only"><?php echo $sale['truck']; ?></td>
+							<td data-title="Cusomer"><?php echo $sale['name'].'<br/><font class="desktop-only">'.$sale['phone'].'</font>'; ?></td>
+							<td data-title="Remarks" class="desktop-only"><?php echo $sale['remarks']; ?></td>
+							<td data-title="Address" class="desktop-only"><?php echo $sale['address']; ?></td>
 						</tr>																																		<?php				
 					}																																				?>
 				</tbody>	
