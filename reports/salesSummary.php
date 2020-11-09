32c3daba225af2f22867edc4c99891aad4207082
nishan <nishanahmad@gmail.com>
2020-11-09 23:37:02 +0530

    Responsive report page

--- reports/salesSummary.php
+++ reports/salesSummary.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <?php
 session_start();
 if(isset($_SESSION["user_name"]))
@@ -77,177 +78,175 @@ if(isset($_SESSION["user_name"]))
 		echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URL . '">';				
 	}	
 ?>
-<html>
-<head>
-	<title>Sales Summary AR</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="../css/styles.css" rel="stylesheet" type="text/css">
-	<link href="../css/navbarMobile.css" media="screen and (max-device-width: 768px)" rel="stylesheet" type="text/css">
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" rel="stylesheet" type="text/css">
-	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.3/js/jquery.tablesorter.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.3/js/jquery.tablesorter.widgets.min.js"></script>
-	<script>
-	$(function() {
-		var pickerOpts = { dateFormat:"dd-mm-yy"}; 
-		$( "#fromDate" ).datepicker(pickerOpts);
-		
-		var pickerOpts2 = { dateFormat:"dd-mm-yy"}; 
-		$( "#toDate" ).datepicker(pickerOpts2);		
+	<html>
+		<head>
+			<title>Sales Summary AR</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link href="../css/styles.css" rel="stylesheet" type="text/css">
+			<link href="../css/navbarMobile.css" media="screen and (max-device-width: 768px)" rel="stylesheet" type="text/css">
+			<link href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" rel="stylesheet" type="text/css">
+			<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+			<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.3/js/jquery.tablesorter.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.3/js/jquery.tablesorter.widgets.min.js"></script>
+			<script>
+			$(function() {
+				var pickerOpts = { dateFormat:"dd-mm-yy"}; 
+				$( "#fromDate" ).datepicker(pickerOpts);
+				
+				var pickerOpts2 = { dateFormat:"dd-mm-yy"}; 
+				$( "#toDate" ).datepicker(pickerOpts2);		
 
-		$(".maintable").tablesorter(); 
-		var $table = $('.maintable');
-	});
-	</script>	
-    <style> 
-		.green{
-			font-weight:bold;
-			font-style:italic;
-			color:LimeGreen			
-		}
-	</style> 
-	
-</head>
-<body>
-<nav class="navbar navbar-light bg-light sticky-top bottom-nav">
-	<span class="navbar-brand" style="font-size:25px;margin-left:40%"><i class="fa fa-line-chart"></i> Summary Report</span>
-</nav>
-<div align="center">
-<br/><br/>
-<form method="post" action="" autocomplete="off">
-	<div class="row" style="margin-left:20%">
-		<div style="width:220px;">
-			<div class="input-group">
-				<span class="input-group-text col-md-5"><i class="far fa-calendar-alt"></i>&nbsp;From</span>
-				<input type="text" required name="fromDate" id="fromDate" class="form-control" autocomplete="off" value="<?php echo date('d-m-Y',strtotime($fromDate)); ?>">
+				$(".maintable").tablesorter(); 
+				var $table = $('.maintable');
+			});
+			</script>	
+			<style> 
+				.green{
+					font-weight:bold;
+					font-style:italic;
+					color:LimeGreen			
+				}
+			</style> 
+			
+		</head>
+		<body>
+			<nav class="navbar navbar-light bg-light sticky-top bottom-nav justify-content-center">
+				<span class="navbar-brand" style="font-size:25px;"><i class="fa fa-line-chart"></i> Summary Report</span>
+			</nav>
+			<div style="width:100%;" class="mainbody">	
+				<br/><br/>
+				<div align="center">
+					<form method="post" action="" autocomplete="off">
+						<div class="row" style="margin-left:20%">
+							<div style="width:220px;">
+								<div class="input-group">
+									<span class="input-group-text col-md-5"><i class="far fa-calendar-alt"></i>&nbsp;From</span>
+									<input type="text" required name="fromDate" id="fromDate" class="form-control" autocomplete="off" value="<?php echo date('d-m-Y',strtotime($fromDate)); ?>">
+								</div>
+							</div>
+							<div style="width:220px;">
+								<div class="input-group">
+									<span class="input-group-text col-md-5"><i class="far fa-calendar-alt"></i>&nbsp;To</span>
+									<input type="text" required name="toDate" id="toDate" class="form-control" value="<?php echo date('d-m-Y',strtotime($toDate)); ?>">
+								</div>
+							</div>
+							<div style="width:220px;">
+								<div class="input-group">
+									<span class="input-group-text col-md-6"><i class="fa fa-shield"></i>&nbsp;Product</span>
+										<select name="product" id="product" required class="form-control">
+											<option value="all">ALL</option>																<?php
+											foreach($products as $pro) 
+											{																								?>
+												<option <?php if($product == $pro['id']) echo 'selected';?> value="<?php echo $pro['id'];?>"><?php echo $pro['name'];?></option>		<?php	
+											}																								?>
+										</select>					
+								</div>
+							</div>
+							<div style="width:220px;">
+								<div class="input-group">
+									<span class="input-group-text col-md-5"><i class="fa fa-address-card-o"></i>&nbsp;Type</span>
+										<select name="type" id="type" required class="form-control">
+											<option <?php if($type == 'all') echo 'selected';?> value="all">ALL</option>
+											<option <?php if($type == 'AR/SR') echo 'selected';?> value="AR/SR">AR</option>
+											<option <?php if($type == 'Engineer') echo 'selected';?> value="Engineer">Engineers</option>
+										</select>					
+								</div>
+							</div>		
+						</div>
+						<br/>
+						<div class="justify-content-center">
+							<input type="submit" class="btn" style="background-color:#54698D;color:white;" value="Search">		
+						</div>			
+					</form>																													<br><?php 
+					if($tallyFlag)
+					{																															?>
+						<a href="tallyVerification.php?date=<?php echo $toDate;?>" class="btn" style="background-color:#E6717C;color:white;float:right;margin-right:30px;">Verify Individual Sale</a><?php
+					}																																												  ?>
+					<br/>
+					<div class="col-md-6 table-responsive-sm">
+					<table class="maintable table table-hover table-bordered table-responsive">
+						<thead>
+							<tr class="table-success">
+								<th style="text-align:left;" class="header" scope="col"><i class="fa fa-map-o"></i> AR</th>
+								<th style="width:12%;text-align:center" class="header" scope="col"><i class="fab fa-buffer"></i> Qty</th>
+								<th style="text-align:left;" class="header" scope="col"><i class="fas fa-store"></i> Shop Name</th>	
+								<th style="width:15%;" class="header" scope="col"><i class="fa fa-mobile"></i> Phone</th>	<?php
+								if($tallyFlag)
+								{																								?>
+									<th class="header" scope="col">VerifiedBy</th>												<?php
+								}																								?>
+							</tr>
+						</thead>																								
+						<tbody class="tablesorter-no-sort">																		<?php
+						$total = 0;
+						foreach($salesList as $arSale)
+						{																										?>
+							<tr id="<?php echo $arNameMap[$arSale['ar_id']];?>">
+								<td style="text-align:left;"><?php echo $arNameMap[$arSale['ar_id']];?></td>
+								<td style="text-align:center"><b><?php echo $arSale['SUM(qty)'];?></b></td>
+								<td style="text-align:left;"><?php echo $arShopMap[$arSale['ar_id']];?></td>			
+								<td><?php echo $arPhoneMap[$arSale['ar_id']];?></td>										<?php
+								if($tallyFlag == true)
+								{		
+									if(isset($tallyMap[$arSale['ar_id']]))
+									{		
+										$userId = $tallyMap[$arSale['ar_id']];																									?>
+										<td><font style="font-weight:bold;font-style:italic;"><?php echo $userMap[$userId];?></font></td>										<?php
+									}
+									else
+									{																																			?>
+										<td><button class="btn" value="<?php echo $arSale['ar_id'];?>" style="background-color:#E6717C;color:white;" onclick="callAjax(this.value)">Verify</button></td>																											<?php			
+									}
+								}																																				?>																																
+							</tr>																																				<?php	
+							$total = $total + $arSale['SUM(qty)'] - $arSale['SUM(return_bag)'];
+						}																																						?>	
+							<tr style="line-height:50px;background-color:#BEBEBE !important;font-family: Arial Black;">
+								<td colspan="2" style="text-align:right" >TOTAL</td>
+								<td colspan="1"><?php echo $total;?></td>
+								<td></td>
+							</tr>
+						</tbody>
+					</table>
+					</div>
+				</div>
+				<br/><br/><br/>
 			</div>
-		</div>
-		<div style="width:220px;">
-			<div class="input-group">
-				<span class="input-group-text col-md-5"><i class="far fa-calendar-alt"></i>&nbsp;To</span>
-				<input type="text" required name="toDate" id="toDate" class="form-control" value="<?php echo date('d-m-Y',strtotime($toDate)); ?>">
-			</div>
-		</div>
-		<div style="width:220px;">
-			<div class="input-group">
-				<span class="input-group-text col-md-6"><i class="fa fa-shield"></i>&nbsp;Product</span>
-					<select name="product" id="product" required class="form-control">
-						<option value="all">ALL</option>																<?php
-						foreach($products as $pro) 
-						{																								?>
-							<option <?php if($product == $pro['id']) echo 'selected';?> value="<?php echo $pro['id'];?>"><?php echo $pro['name'];?></option>		<?php	
-						}																								?>
-					</select>					
-			</div>
-		</div>
-		<div style="width:220px;">
-			<div class="input-group">
-				<span class="input-group-text col-md-5"><i class="fa fa-address-card-o"></i>&nbsp;Type</span>
-					<select name="type" id="type" required class="form-control">
-						<option <?php if($type == 'all') echo 'selected';?> value="all">ALL</option>
-						<option <?php if($type == 'AR/SR') echo 'selected';?> value="AR/SR">AR</option>
-						<option <?php if($type == 'Engineer') echo 'selected';?> value="Engineer">Engineers</option>
-					</select>					
-			</div>
-		</div>		
-	</div>
-	<br/>
-	<div class="col col-md-2 offset-1">
-		<div class="input-group">		
-			<input type="submit" class="btn" style="background-color:#54698D;color:white;" value="Search">		
-		</div>
-	</div>			
-	<br/><br/>
-</form>																													<br><?php 
-if($tallyFlag)
-{																															?>
-	<a href="tallyVerification.php?date=<?php echo $toDate;?>" class="btn" style="background-color:#E6717C;color:white;float:right;margin-right:30px;">Verify Individual Sale</a><?php
-}																																												  ?>
+			<script>
+				function callAjax(ar){
+					const queryString = window.location.search;
+					const urlParams = new URLSearchParams(queryString);
+					var date = urlParams.get('to');
+					if(!date)
+					{
+						date = new Date();
+						var dd = String(date.getDate()).padStart(2, '0');
+						var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+						var yyyy = date.getFullYear();
 
-<table class="maintable table table-hover table-bordered" style="width:50%;margin-left:40px;">
-<thead style="position: sticky;top: 0">
-	<tr class="table-success">
-		<th style="text-align:left;" class="header" scope="col"><i class="fa fa-map-o"></i> AR</th>
-		<th style="text-align:left;" class="header" scope="col"><i class="fas fa-store"></i> Shop Name</th>	
-		<th style="width:12%;" class="header" scope="col"><i class="fa fa-address-card-o"></i> SAP</th>	
-		<th style="width:15%;" class="header" scope="col"><i class="fa fa-mobile"></i> Phone</th>
-		<th style="width:12%;text-align:center" class="header" scope="col"><i class="fab fa-buffer"></i> Qty</th>					<?php
-		if($tallyFlag)
-		{																								?>
-			<th class="header" scope="col">VerifiedBy</th>				<?php
-		}																								?>
-	</tr>
-</thead>
-<?php
-	$total = 0;
-	foreach($salesList as $arSale)
-	{
-?>		<tr id="<?php echo $arNameMap[$arSale['ar_id']];?>">
-			<td style="text-align:left;"><?php echo $arNameMap[$arSale['ar_id']];?></td>
-			<td style="text-align:left;"><?php echo $arShopMap[$arSale['ar_id']];?></td>			
-			<td><?php echo $arCodeMap[$arSale['ar_id']];?></td>			
-			<td><?php echo $arPhoneMap[$arSale['ar_id']];?></td>						
-			<td style="text-align:center"><b><?php echo $arSale['SUM(qty)'];?></b></td>																		<?php
-			if($tallyFlag == true)
-			{		
-				if(isset($tallyMap[$arSale['ar_id']]))
-				{		
-					$userId = $tallyMap[$arSale['ar_id']];																									?>
-					<td><font style="font-weight:bold;font-style:italic;"><?php echo $userMap[$userId];?></font></td>										<?php
+						date = dd + '-' + mm + '-' + yyyy;
+					}
+					$.ajax({
+						type: "POST",
+						url: "ajax/updateDayTally.php",
+						data:'ar='+ar +'&date='+date,
+						success: function(response){
+							if(response != false){
+								$('#'+response).find('td').eq(5).text('VERIFIED!');
+								$('#'+response).find('td').eq(5).addClass("green")
+							}
+							else{
+								alert('Some error occured. Try again');
+								location.reload();
+							}
+						}
+					});	  
 				}
-				else
-				{																																			?>
-					<td><button class="btn" value="<?php echo $arSale['ar_id'];?>" style="background-color:#E6717C;color:white;" onclick="callAjax(this.value)">Verify</button></td>																											<?php			
-				}
-			}																																				?>																																
-		</tr>
-<?php	
-		$total = $total + $arSale['SUM(qty)'] - $arSale['SUM(return_bag)'];
-	}
-?>	
-	<tbody class="tablesorter-no-sort">
-		<tr style="line-height:50px;background-color:#BEBEBE !important;font-family: Arial Black;">
-			<td colspan="4" style="text-align:right" >TOTAL</td>
-			<td colspan="2"><?php echo $total;?></td>
-		</tr>
-	</tbody>
-</table>
-<br/><br/><br/>
-</div>
-<script>
-	function callAjax(ar){
-		const queryString = window.location.search;
-		const urlParams = new URLSearchParams(queryString);
-		var date = urlParams.get('to');
-		if(!date)
-		{
-			date = new Date();
-			var dd = String(date.getDate()).padStart(2, '0');
-			var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
-			var yyyy = date.getFullYear();
-
-			date = dd + '-' + mm + '-' + yyyy;
-		}
-		$.ajax({
-			type: "POST",
-			url: "ajax/updateDayTally.php",
-			data:'ar='+ar +'&date='+date,
-			success: function(response){
-				if(response != false){
-					$('#'+response).find('td').eq(5).text('VERIFIED!');
-					$('#'+response).find('td').eq(5).addClass("green")
-				}
-				else{
-					alert('Some error occured. Try again');
-					location.reload();
-				}
-			}
-		});	  
-	}
-</script>
-</body>			
-<?php
+			</script>
+		</body>	
+	</html>																																											<?php
 }
 else
 	header("Location:../index.php");	
