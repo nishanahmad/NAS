46ccdfced952d3183e625cf63c7aee17fc586c72
nishan <nishanahmad@gmail.com>
2020-12-10 14:54:49 +0530

    Home

---
+++ index/dashboard.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+session_start();
+
+if(isset($_SESSION["user_name"]))
+{
+	require '../connect.php';
+	require '../navbar.php';
+	require '../sales/listHelper.php';   // imported to get clientNamesMap and productDetailsMap;																?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Home</title>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+</head>
+
+<body id="page-top">
+    <div id="wrapper">
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                <div class="container-fluid">
+					<br/><br/>
+                    <div class="row">
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Earnings (Monthly)</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-success shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-info shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
+                                            <div class="row no-gutters align-items-center">
+                                                <div class="col-auto">
+                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                                </div>
+                                                <div class="col">
+                                                    <div class="progress progress-sm mr-2">
+                                                        <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-warning shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Pending Requests</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-comments fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xl-8 col-lg-7">
+                            <div class="card shadow mb-4">
+                                <div
+                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-primary">Weekly Sales Overview</h6>
+                                </div>
+                                <div class="card-body">
+                                    <div class="chart-area">
+                                        <canvas id="myAreaChart"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-lg-5">
+                            <div class="card shadow mb-4">
+                                <div
+                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-primary">Current Bill Status</h6>
+                                    <div class="dropdown no-arrow">
+                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <div class="chart-pie pt-4 pb-2">
+                                        <canvas id="myPieChart"></canvas>
+                                    </div>
+                                    <div class="mt-4 text-center small">
+                                        <span class="mr-2">
+                                            <i class="fas fa-circle text-primary"></i> Direct
+                                        </span>
+                                        <span class="mr-2">
+                                            <i class="fas fa-circle text-success"></i> Social
+                                        </span>
+                                        <span class="mr-2">
+                                            <i class="fas fa-circle text-info"></i> Referral
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+	<script src="../js/jquery.easing.min.js"></script>
+    <script src="../js/sb-admin-2.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+	<script src="../js/chart-pie-demo.js"></script>
+</body>
+
+</html>																																					<?php
+}
+else
+	header("Location:../sessions/loginPage.php");																													?>
--- index/home.php
+++ index/home.php
@@ -162,7 +162,7 @@ if(isset($_SESSION["user_name"]))
 			<span class="navbar-brand offset-5" style="font-size:25px;"><i class="fa fa-home"></i> Home</span>
 		</nav>	
 		<br/><br/>
-		<div id="content-mobile">
+		<div>
 			<div class="container">	
 				<div class="col-8">
 					<div class="row no-gutters">
@@ -215,74 +215,6 @@ if(isset($_SESSION["user_name"]))
 				</div>			
 			</div>
 		</div>
-		<div>
-			<div class="graphcontainer">	
-			  <div class="box">
-				<div class="chart" data-percent="<?php echo $percentage;?>" data-scale-color="#ffb400"><?php echo $percentage;?>%</div>
-				<h2><?php echo 'Billed '.$billedCount.'/'; echo $billedCount + $unbilledCount;?></h2>
-			  </div>
-			  <div class="box">
-				<div class="chart" data-percent="90" data-scale-color="#ffb400">90%</div>
-				<h2>Css</h2>
-			  </div>
-			  <div class="box">
-				<div class="chart" data-percent="85" data-scale-color="#ffb400">85%</div>
-				<h2>Javascript</h2>
-			  </div>
-			  <div class="box">
-				<div class="chart" data-percent="95" data-scale-color="#ffb400">95%</div>
-				<h2>Photoshop</h2>
-			  </div>
-			</div>
-
-			<div class="card" style="width:50%;">
-			  <div class="card-body">
-				<canvas id="bar-chart"></canvas>
-			  </div>
-			</div>
-			
-			
-		<br/><br/><br/><br/>
-		<script>
-			$(function() {
-			  $('.chart').easyPieChart({
-				size: 160,
-				barColor: "#17d3e6",
-				scaleLength: 0,
-				lineWidth: 15,
-				trackColor: "#373737",
-				lineCap: "circle",
-				animate: 2000,
-			  });			  
-			});
-		
-			new Chart(document.getElementById("bar-chart"), {
-				type: 'bar',
-				data: {
-				  labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-				  datasets: [
-					{
-					  label: "Population (millions)",
-					  backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-					  data: [2478,5267,734,784,433]
-					}
-				  ]
-				},
-				options: {
-					scales: {
-						xAxes: [{
-						barThickness: 20,  // number (pixels) or 'flex'
-						maxBarThickness: 20 // number (pixels)
-						}]
-					},					
-				  legend: { display: false },
-				  title: {
-					display: true,
-					text: 'Predicted world population (millions) in 2050'
-				  }
-				}
-			});
-		</script>
 	</body>
 </html>																																					<?php
 }
