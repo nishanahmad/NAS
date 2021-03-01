062339cc457d1a6eb8ff9536d9630f36e3e333f2
nishan <nishanahmad@gmail.com>
2021-03-01 17:09:46 +0530

    EOD

--- test.php
+++ test.php
@@ -1,12 +1,20 @@
 <?php
+//
+// A very simple PHP example that sends a HTTP POST to a remote site
+//
 
-function getWeekMonSun($weekOffset) {
-    $dt = new DateTime();
-    $dt->setIsoDate($dt->format('o'), $dt->format('W') + $weekOffset);
-    return array(
-        'Mon' => $dt->format('Y-m-d'),
-        'Sun' => $dt->modify('+5 day')->format('Y-m-d'),
-    );
-}
+$ch = curl_init();
 
-var_dump(getWeekMonSun(-1));
+curl_setopt($ch, CURLOPT_URL,"https://nassales.in/api/retail/create.php");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query(array('entry_date' => '2021-03-10','product' => '1','qty' => '100','ar_id' => '1','customer_name' => 'Nishan','customer_phone' => '9846086862','address1' => 'Test','entered_by' => 'Nishan')));
+
+// Receive server response ...
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$server_output = curl_exec($ch);
+
+curl_close ($ch);
+
+var_dump($server_output);
+?>
