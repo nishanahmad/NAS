e2c3c8e8b26f7ba571b7a39f48a9790f3dd07a59
nishan <nishanahmad@gmail.com>
2021-06-29 00:19:50 +0530

    Sheet report

--- Sheet/dayBook.php
+++ Sheet/dayBook.php
@@ -23,10 +23,10 @@ if(isset($_SESSION["user_name"]))
 				
 		$areaList = implode("','",array_keys($areaList));
 		
-		$agr1 = mysqli_query($con,"SELECT count(id) FROM sheets WHERE delivered_on ='$date' AND delivered_by = $driverId AND status ='delivered'" ) or die(mysqli_error($con));
+		$agr1 = mysqli_query($con,"SELECT count(id) FROM sheets WHERE delivered_on ='$date' AND delivered_by = $driverId" ) or die(mysqli_error($con));
 		$delivered = (int)mysqli_fetch_Array($agr1,MYSQLI_ASSOC)['count(id)'];
 
-		$agr2 = mysqli_query($con,"SELECT count(id) FROM sheets WHERE closed_on ='$date' AND closed_by = $driverId  AND status ='closed'" ) or die(mysqli_error($con));
+		$agr2 = mysqli_query($con,"SELECT count(id) FROM sheets WHERE closed_on ='$date' AND closed_by = $driverId" ) or die(mysqli_error($con));
 		$taken = (int)mysqli_fetch_Array($agr2,MYSQLI_ASSOC)['count(id)'];		
 		
 		$agr3 = mysqli_query($con,"SELECT count(id) FROM sheets WHERE date ='$date' AND driver_area IN ('$areaList') AND status ='requested'" ) or die(mysqli_error($con));
