fccaf48fcbca05c944fe5249d23916f660d77f42
Nishan <nishanahmad@gmail.com>
2021-07-17 12:05:30 +0530

    Bug fixes Aneesh issue

--- Sheet/close.php
+++ Sheet/close.php
@@ -22,12 +22,12 @@ if(isset($_SESSION["user_name"]))
 
 	if($qty == $originalQty)
 	{
-		$updateQuery = mysqli_query($con,"UPDATE sheets SET status ='closed', closed_on='$sqlDate', closed_by = $closed_by WHERE id=$id ") or die(mysqli_error($con));	
+		$updateQuery = "UPDATE sheets SET status ='closed', closed_on='$sqlDate', closed_by = $closed_by WHERE id=$id";
 	}
 	else if($qty < $originalQty)
 	{
 		$diff = $originalQty - $qty;
-		$updateQuery = mysqli_query($con,"UPDATE sheets SET qty = $diff WHERE id=$id ") or die(mysqli_error($con));	
+		$updateQuery = "UPDATE sheets SET qty = $diff WHERE id=$id ";
 	}
 	else
 	{
@@ -35,29 +35,31 @@ if(isset($_SESSION["user_name"]))
 		exit;
 	}
 	
+	$update = mysqli_query($con,$updateQuery);
+	if($update)
+	{
+		/********************				UPDATE SHEETS IN HAND FOR THE USER				********************/
+		$selectUser = mysqli_query($con,"SELECT * FROM sheets_in_hand WHERE user=$closed_by ") or die(mysqli_error($con));
+		
+		if(mysqli_num_rows($selectUser) > 0)
+		{
+			$update = mysqli_query($con,"UPDATE sheets_in_hand SET qty =qty + $qty WHERE user=$closed_by ") or die(mysqli_error($con));		
+		}
+		else
+		{
+			$insert = mysqli_query($con,"INSERT INTO sheets_in_hand (user, qty) VALUES ($closed_by, $qty)") or die(mysqli_error($con));
+		}
 
+		$queryTo = mysqli_query($con,"SELECT qty FROM sheets_in_hand WHERE user=$closed_by ") or die(mysqli_error($con));
+		$toStock = mysqli_fetch_array($queryTo,MYSQLI_ASSOC)['qty'];
+
+		$transferred_on = date('Y-m-d H:i:s');
+		$transferred_by = $_SESSION['user_id'];		
+
+		$insertLogs = mysqli_query($con, "INSERT INTO transfer_logs (user_to, qty, transferred_on, transferred_by, toStock, site) VALUES ('$closed_by', '$qty', '$transferred_on', '$transferred_by', '$toStock', '$id')") or die(mysqli_error($con));		
+	}		
 
 	
-	/********************				UPDATE SHEETS IN HAND FOR THE USER				********************/
-	$selectUser = mysqli_query($con,"SELECT * FROM sheets_in_hand WHERE user=$closed_by ") or die(mysqli_error($con));
-	
-	if(mysqli_num_rows($selectUser) > 0)
-	{
-		$update = mysqli_query($con,"UPDATE sheets_in_hand SET qty =qty + $qty WHERE user=$closed_by ") or die(mysqli_error($con));		
-	}
-	else
-	{
-		$insert = mysqli_query($con,"INSERT INTO sheets_in_hand (user, qty) VALUES ($closed_by, $qty)") or die(mysqli_error($con));
-	}
-
-	$queryTo = mysqli_query($con,"SELECT qty FROM sheets_in_hand WHERE user=$closed_by ") or die(mysqli_error($con));
-	$toStock = mysqli_fetch_array($queryTo,MYSQLI_ASSOC)['qty'];
-
-	$transferred_on = date('Y-m-d H:i:s');
-	$transferred_by = $_SESSION['user_id'];		
-
-	$insertLogs = mysqli_query($con, "INSERT INTO transfer_logs (user_to, qty, transferred_on, transferred_by, toStock, site) VALUES ('$closed_by', '$qty', '$transferred_on', '$transferred_by', '$toStock', '$id')") or die(mysqli_error($con));	
-
 	header( "Location: deliveries.php?");
 }
 else
