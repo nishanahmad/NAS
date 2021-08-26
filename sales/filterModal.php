62550d524b718fa87b5b71bee84f83655044cf01
Nishan <nishanahmad@gmail.com>
2021-08-26 11:54:13 +0530

    Minor fix

--- sales/filterModal.php
+++ sales/filterModal.php
@@ -5,8 +5,7 @@ error_reporting(E_ALL);
 require '../connect.php';
 if(isset($_SESSION["user_name"]))
 {			
-	$clients = mysqli_query($con,"SELECT id,name FROM ar_details WHERE type= 'AR/SR' OR Type = 'Engineer' ORDER BY name ASC");
-	$engineers = mysqli_query($con,"SELECT id,name FROM ar_details WHERE Type = 'Engineer Only' ORDER BY name ASC");
+	$clients = mysqli_query($con,"SELECT id,name FROM ar_details ORDER BY name ASC");
 	$products= mysqli_query($con,"SELECT id,name FROM products ORDER BY id ASC");																?>
 	
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
