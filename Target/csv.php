8348af73601e229f9618dd036014262065905770
Nishan <nishanahmad@gmail.com>
2021-12-21 12:03:37 +0530

    Office

--- Target/csv.php
+++ Target/csv.php
@@ -20,12 +20,12 @@ ini_set('memory_limit', '-1');
 				foreach ($csvFile as $str)
 				{
 					$row = explode(",",$str);
-					$message = "DEAR AR, YOUR BALANCE TO ACHEIVE SPECIAL TARGET OF 21st TO 30th Nov IS ".$row[1]." BAGS.ACHEIVE YOUR TARGET & EARN SPECIAL BENEFITS - AR HELP";
+					$message = "DEAR AR,YOUR BALANCE TO ACHIEVE YOUR SPECIAL TARGET OF 11th TO 18th Dec IS ".$row[1]." BAGS. ACHIEVE YOUR TARGET , EARN SPECIAL BENEFITS & LAKSHYA BENIFITS - AR HELP";
 					$phone = '91'.$row[0];
 					echo $phone.'<br/>';
 					echo $message.'<br/>';
 					sleep(2);
-					//$status = sendMessage($message,$phone);
+					$status = sendMessage($message,$phone);
 				}
 			}
 		?>
