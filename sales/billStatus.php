d93b191c61b770aa3bc4d110dc63dd512d42a45f
Nishan Ahmed <nishanahmad@gmail.com>
2022-04-07 22:49:50 +0530

    Home

--- sales/billStatus.php
+++ sales/billStatus.php
@@ -2,7 +2,7 @@
 
 function billStatus($bill)
 {	
-	if( fnmatch("B*",$bill) || fnmatch("C*",$bill) || fnmatch("GB*",$bill) || fnmatch("GC*",$bill) || fnmatch("PB*",$bill) || fnmatch("PC*",$bill))
+	if( fnmatch("B*",$bill) || fnmatch("C*",$bill) || fnmatch("D*",$bill) || fnmatch("GB*",$bill) || fnmatch("GC*",$bill) || fnmatch("PB*",$bill) || fnmatch("PC*",$bill) || fnmatch("TRF*",$bill))
 		return true;
 	else
 		return false;
