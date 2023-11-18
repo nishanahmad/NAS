d5a8f5184c089986651660e68ced813db09bcde0
Nishan Ahmed <nishanahmad@gmail.com>
2023-11-18 10:25:27 +0530

    PC

--- UPN Messaging/sendMessage.php
+++ UPN Messaging/sendMessage.php
@@ -5,8 +5,8 @@ function sendMessage($message,$phone)
 {
 	$uri =  "https://api.wa.anant.io/v1/send";
 	
-	$instanceId = '458f01be-f075-4745-8639-f6632c7a3697';
-	$authToken = '3a9344a4-600e-47b2-ab8a-4c42e64adda7';
+	$instanceId = 'e0f41d66-9142-49c7-ad9e-19bbae820fc5';
+	$authToken = 'd81929dc-7d33-a8c8-2a2b-8cd78e5fd0df';
 
 	$response = \Httpful\Request::post($uri)
                     ->body([
