b97243b08e783ac114615a265e5ae558828ff58c
Nishan Ahmed <nishanahmad@gmail.com>
2025-11-22 11:15:56 +0530

    PC

--- sidebar_acc.php
+++ sidebar_acc.php
@@ -17,7 +17,7 @@ if (strpos($url, '/redemption') !== false)
 		<a href="../ar/list.php?brand=ut"><img style="margin-left:40px;" src="../images/acc_logo.png"></a>
 		<ul>
 			<li <?php if ($flag == 'ar') echo 'class="active"';?>><a href="../ar/list.php?brand=acc">AR List</a></li>
-			<li <?php if ($flag == 'target') echo 'class="active"';?>><a <?php if ($flag == 'target') {echo 'href="#"';} else{echo 'href="../Target/acc_list.php"';}?>>Target</a></li>
+			<li <?php if ($flag == 'target') echo 'class="active"';?>><a <?php if ($flag == 'target') {echo 'href="#"';} else{echo 'href="../Target/acc_list.php?"';}?>>Target</a></li>
 			<li <?php if ($flag == 'SpecialTarget') echo 'class="active"';?>><a <?php if ($flag == 'SpecialTarget') {echo 'href="#"';} else{echo 'href="../SpecialTarget/acc_list.php?"';}?>>Special Target</a></li>
 			<li <?php if ($flag == 'gold_points') echo 'class="active"';?>><a <?php if ($flag == 'gold_points') {echo 'href="#"';} else{echo 'href="../gold_points/acc_list.php?"';}?>>Gold Points</a></li>
 			<li <?php if ($flag == 'redemption') echo 'class="active"';?>><a <?php if ($flag == 'redemption') {echo 'href="#"';} else{echo 'href="../redemption/acc_list.php?"';}?>>Redemption</a></li>
